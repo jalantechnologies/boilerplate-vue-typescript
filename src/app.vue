68b0809788ee8c6461ae3598d1521603ad92f403
Rajat Deepak Vindru <rajatvindr000@gmail.com>
2022-02-09 22:39:46 +0530

    feat: init vue3 project with TS, SCSS and Jest

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld msg="Welcome to Vue 3 App" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "./components/hello-world/index.vue";
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
