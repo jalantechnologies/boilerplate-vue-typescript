c9f543b154467628e400d62d8cb630af6229bd13
Rajat Deepak Vindru <rajatvindr000@gmail.com>
2022-02-10 22:19:43 +0530

    feat: removin logo

--- src/components/base-label/index.vue
+++ src/components/base-label/index.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="flex-col d-flex flex-column align-items-center">
-    <img
-      :src="require('@/assets/logo.png')"
-      style="height: 100px; width: 100px"
-    />
     <p class="h1"><slot /></p>
   </div>
 </template>
