664daea650bc189b361dbe6e9d12e87711f14610
shrinidhi <shrinidhi.uppoor@jalantechnologies.com>
2022-02-11 16:30:18 +0530

    test:cypress

---
+++ tests/e2e/plugins/index.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  require('cypress-mochawesome-reporter/plugin');
+
+  return Object.assign({}, config, {
+    fixturesFolder: 'tests/e2e/fixtures',
+    integrationFolder: 'tests/e2e/specs',
+    screenshotsFolder: 'tests/e2e/screenshots',
+    videosFolder: 'tests/e2e/videos',
+    supportFile: 'tests/e2e/support/index.js'
+  })
+}
